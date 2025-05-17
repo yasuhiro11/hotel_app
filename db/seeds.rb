1b6b08a523e8b3515b31e69212fe83ba516ed750
hanzawa825 <157540704+hanzawa825@users.noreply.github.com>
2025-05-17 22:17:37 +0900

    add/sampleホテルの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Property.find_or_create_by!(title: "大阪プレミアムホテル") do |p|
+  p.description = "大阪にある高級ホテルです。"
+  p.price = 8232
+end
+
+Property.find_or_create_by!(title: "東京ラグジュアリーホテル") do |p|
+  p.description = "東京にあるラグジュアリーホテルです。"
+  p.price = 12431
+end
+
+Property.find_or_create_by!(title: "札幌ビューホテル") do |p|
+  p.description = "札幌の絶景を楽しめるホテルです。"
+  p.price = 12468
+end
